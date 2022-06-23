0af40f0d8d90ff427b5a853ab7b7c1709073d7a4
Zuge Gao <audreygaozu@hotmail.com>
2022-06-23 02:32:36 +0000

    add toggle strench

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,44 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const load = function () {
-  $.ajax({
-    method: "GET",
-    url: "http://localhost:8080/tweets",
-  }).then(function (tweet) {
-    renderTweets(tweet);
-    //resets the form
-    document.querySelector(".form").reset();
-  });
-};
-
-// $(".form").submit(function (event){
-//   event.preventDefault();
-//   // const newTweet = event.target[0].value;
-//   $.ajax({
-//     method: "POST",
-//     url: "http://localhost:8080/tweets",
-//     data: $(this).serialize()
-//   }).then(function () {
-//     load();
-//   });
-// });
-
-const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  const $tweets_container = $('#tweets_container');
-  for (const i of tweets) {
-    $tweets_container.prepend(createTweetElement(i));
-
-    
-  }
-  return $tweets_container;
-};
 
 const createTweetElement = function(obj){
-  let $tweet = (`
+  let $tweet = `
   <article class="tweet">
       <header>
         <div class="user">
@@ -58,60 +23,65 @@ const createTweetElement = function(obj){
       </main>
       <footer>
         <div>
-          <p>${obj.created_at}</p>
+          <p>${timeago.format(obj.created_at)}</p>
         </div>
         <div class="symbol"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-repeat"></i><i class="fa-solid fa-heart"></i></div>
       </footer>
-  </article>`)
+  </article>`
   return $tweet;
 
 };
 
-
-
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  
+  $('#tweets_container').empty();
+  for (const i of tweets) {
+    $('#tweets_container').prepend(createTweetElement(i));
   }
-];
+  
+};
 
-
+const loadTweets = function () {
+  $.ajax({
+    method: "GET",
+    url: "/tweets/",
+  }).then(function (tweet) {
+    renderTweets(tweet);
+  });
+};
 
 $(document).ready(() => {
-  load();
+  loadTweets();  
   $(".form").submit(function (event){
+    
     event.preventDefault();
-    // const newTweet = event.target[0].value;
+    const info = event.target[0].value;
+    console.log(this);
+    if (!info) {
+      return $(".error").text(` ⚠️ Sorry, you can not make an empty post ⚠️`).slideDown();
+  
+    }
+    if (info.length > 140) {
+      return $(".error").text('⚠️ Your Tweet exceeds the max character limit ⚠️').slideDown();
+    }
+    
     $.ajax({
       method: "POST",
-      url: "http://localhost:8080/tweets",
-      data: $(this).serialize()
+      url: "/tweets/",
+      data: $("#tweet-text").serialize(),
     }).then(function () {
-      load();
+      $(".error").hide();
+      loadTweets();
     });
+    $('#tweet-text').val('');
     $('.counter').text(140);
-  });// to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    
+  });
+
+  $("#down_arrow").click(function(){
+    $(".new-tweet").slideToggle();
+  });
 });
